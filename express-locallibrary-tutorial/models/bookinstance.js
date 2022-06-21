1e18110c7290ff827548db00db27490d2c08225f
Mario De Leon <mariodeleon3701@gmail.com>
2022-06-21 13:04:57 -0500

    Todas la paginas de solo lectura han sido creadas

--- express-locallibrary-tutorial/models/bookinstance.js
+++ express-locallibrary-tutorial/models/bookinstance.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+const { DateTime } = require("luxon");
+
 
 var Schema = mongoose.Schema;
 
@@ -12,6 +14,12 @@ var BookInstanceSchema = new Schema(
 );
 
 // Virtual for bookinstance's URL
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+});
+
 BookInstanceSchema
 .virtual('url')
 .get(function () {
