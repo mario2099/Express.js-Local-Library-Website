29424d666a84370d1967d40c0d515a9f0f557fc1
Mario De Leon <mariodeleon3701@gmail.com>
2022-07-05 15:56:26 -0500

    ..

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -20,9 +20,13 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 
+// Create the Express application object
+var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
 
+app.use(helmet());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -31,6 +35,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); //Compress all routes
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
